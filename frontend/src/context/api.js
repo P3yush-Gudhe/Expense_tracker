ae135974c8d624a4deb608e7ce24d744539e3639
Piyush Gudhe <piyushgudhe563@gmail.com>
2026-03-15 15:32:54 +0530

    backend connected properly

--- frontend/src/context/api.js
+++ frontend/src/context/api.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: "expensetracker-production-fc51.up.railway.app"});
+const API = axios.create({
+  baseURL: "https://expensetracker-production-fc51.up.railway.app"
+});
 
 API.interceptors.request.use((req) => {
-  const token = localStorage.getItem('token');
-  if (token) req.headers.Authorization = `Bearer ${token}`;
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
+
   return req;
 });
 
