0475dde80c3cfbee6d9996256702cf5baed1dc31
Piyush Gudhe <piyushgudhe563@gmail.com>
2026-03-15 15:42:09 +0530

    frontend fixed

--- frontend/src/context/api.js
+++ frontend/src/context/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: "https://expensetracker-production-fc51.up.railway.app"
+  baseURL: "https://expensetracker-production-fc51.up.railway.app/api"
 });
 
 API.interceptors.request.use((req) => {
